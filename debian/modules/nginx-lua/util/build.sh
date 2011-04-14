f43363d738418f7a88308dd7ca2018769ddb81d0
Kartik Mistry <kartik@debian.org>
2011-04-14 10:41:08 +0530

    Imported Debian patch 1.0.0-1

---
+++ debian/modules/nginx-lua/util/build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# this file is mostly meant to be used by the author himself.
+
+version=${1:-0.8.54}
+opts=$2
+
+script_dir=$(dirname $0)
+root=$(readlink -f $script_dir/..)
+mkdir -p $root/{build,work}
+
+cd $root
+git submodule update
+
+cd $root/build
+if [ ! -s nginx-$version.tar.gz ]; then
+    wget "http://sysoev.ru/nginx/nginx-$version.tar.gz" -O nginx-$version.tar.gz
+fi
+tar -xzvf nginx-$version.tar.gz
+
+cd nginx-$version/
+if [[ "$BUILD_CLEAN" -eq 1 || ! -f Makefile || "$root/config" -nt Makefile || "$root/util/build.sh" -nt Makefile ]]; then
+	./configure --prefix=$root/work \
+            --with-cc-opt="-O0" \
+				--add-module=$root \
+				--add-module=$root/deps/ngx_devel_kit \
+				$opts
+fi
+
+if [ -f $root/work/sbin/nginx ]; then
+    rm -f $root/work/sbin/nginx
+fi
+
+if [ -f $root/work/logs/nginx.pid ]; then
+    kill `cat $root/work/logs/nginx.pid`
+fi
+
+make -j2
+make install
+
